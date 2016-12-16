cf689ad46076ba72411825a1d777c81a61d5156b
Felix Rieseberg <felix@felixrieseberg.com>
2016-12-16 13:24:25 -0800

    :memo: Log result

--- check.js
+++ check.js
@@ -1,14 +1,14 @@
 if (process.env.npm_config_user_agent) {
-  var versionRegex = /npm\/([0-9]{1,3}.[0-9]{1,4}.[0-9]{1,4})/
+  var versionRegex = /npm\/(([0-9]{1,3}).[0-9]{1,4}.[0-9]{1,4})/
   var npmVersion = process.env.npm_config_user_agent.match(versionRegex)
   var npmMajor
 
   if (npmVersion && npmVersion.length > 1) {
-    npmVersion = npmVersion[1]
     npmMajor = parseInt(npmVersion[2], 10)
 
-    if (npmMajor <= 4) {
+    if (npmMajor < 4) {
       console.error(`This package requires at least npm 4!`)
+      console.log('We think that you have npm ' + npmMajor)
 
       if (process.platform === 'win32') {
         console.log('You can install a newer npm by running')
